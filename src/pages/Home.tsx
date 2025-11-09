beb818f248fadf318c91dfaa0f7f256d7ef49385
salomaochamma <raphaelchamma53@gmail.com>
2025-11-09 19:25:14 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,10 @@ export default function Home(){
       <Hero />
 
       <section className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+
+      <section className="grid grid-cols-1 md:grid-cols-3 gap6"
+
         <div className="col-span-2">
           <div className="grid grid-cols-1 gap-6">
             <div className="bg-white p-6 rounded shadow">
